681137c6c3bf311e707ecb587ad747f784d25878
yatish kadam <yatish kadam>
2017-05-12 03:45:43 -0400

    db

--- webdev/test/app.js
+++ webdev/test/app.js
@@ -6,10 +6,10 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
     if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-	var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-	var password = process.env.MLAB_PASSWORD_WEBDEV;
-	connectionString = 'mongodb://' + username + ':' + password;
-	connectionString += ''; // user yours
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += 'mongodb://<dbuser>:<dbpassword>@ds139801.mlab.com:39801/heroku_wqxmccbt'; // user yours
 }
 
     if(process.env.MLAB_USERNAME) {
