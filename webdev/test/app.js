f70a7c6f5040e0b4e9173ced8f0a750661b74c51
yatish kadam <yatish kadam>
2017-05-12 03:52:25 -0400

    db

--- webdev/test/app.js
+++ webdev/test/app.js
@@ -9,8 +9,8 @@ module.exports = function(app)
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += 'mongodb://<dbuser>:<dbpassword>@ds139801.mlab.com:39801/heroku_wqxmccbt'; // user yours
-}
+    connectionString += 'mongodb://<admin>:<admin>@ds139801.mlab.com:39801/heroku_wqxmccbt'; // user yours
+	}
 
     if(process.env.MLAB_USERNAME) {
         connectionString = process.env.MLAB_USERNAME + ":" +
